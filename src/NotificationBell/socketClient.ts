8ab5e77789e50c2929bb549fcffc4fd20865a4fe
anthonyYo03 <antoyou78@gmail.com>
2026-03-08 18:15:14 +0200

    add transports to socket io

--- src/NotificationBell/socketClient.ts
+++ src/NotificationBell/socketClient.ts
@@ -3,8 +3,9 @@ import { io } from 'socket.io-client';
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 
 const socket = io(BACKEND_URL, {
-  withCredentials: true,
+   withCredentials: true,
   autoConnect: true,
+  transports: ['websocket'],
 });
 
 export default socket;
